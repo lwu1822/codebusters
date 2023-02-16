965a1ae3f79e81c01270cd943bd8f8aff98c6b3c
lwu1822 <lwu1822@gmail.com>
2023-02-16 10:23:25 -0800

    revert note

--- src/main/java/com/nighthawk/spring_portfolio/mvc/note/Note.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/note/Note.java
@@ -1,34 +1,48 @@
-package com.nighthawk.spring_portfolio.mvc.cipher.Cipher;
+package com.nighthawk.spring_portfolio.mvc.note;
 
-import lombok.*;
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nighthawk.spring_portfolio.mvc.person.Person;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
-public class Cipher {
+public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotNull
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition="TEXT")
     private String text;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "tutorial_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
-    private Club club;
+    private Person person;
 
-    public Cipher(String text, Cipher p) {
+    public Note(String text, Person person) {
         this.text = text;
-        this.cipher = p;
+        this.person = person;
     }
+
+
 }
--- src/main/java/com/nighthawk/spring_portfolio/mvc/note/NoteJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/note/NoteJpaRepository.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import com.nighthawk.spring_portfolio.mvc.person.Person;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteJpaRepository extends JpaRepository<Note, Long> {
-    List<Note> findByPersonId(Long id);
+    List<Person> findByPersonId(Long id);
 
     @Transactional
     void deleteByPersonId(long id);
-
 }
