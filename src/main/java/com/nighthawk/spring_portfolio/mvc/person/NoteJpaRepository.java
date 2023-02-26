5eac40cb8704893e0cac26932742dfb38747d15e
VidhiKulkarni <vidhi.kulkarni@gmail.com>
2023-02-26 15:22:04 -0800

    notes

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/NoteJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/NoteJpaRepository.java
@@ -3,5 +3,5 @@ package com.nighthawk.spring_portfolio.mvc.person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteJpaRepository extends JpaRepository<Note, Long> {
-    Log findByEmail(String email);
+    Note findByEmail(String email);
 }
