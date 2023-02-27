ea6ea75028df8accc3d455ec8047b4bfb5a27d91
willcyber <wutwilliam@gmail.com>
2023-02-26 21:50:43 -0800

    pls

--- src/main/java/com/nighthawk/spring_portfolio/mvc/vigenere/VigenereEncrypt.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/vigenere/VigenereEncrypt.java
@@ -1,33 +1,37 @@
 package com.nighthawk.spring_portfolio.mvc.vigenere;
 
-public class VigenereEncrypt{
+public class VigenereEncrypt {
     private final String key;
+    private final String text;
 
-    public VigenereEncrypt(String key) {
+    public VigenereEncrypt(String text, String key) {
         this.key = key.toUpperCase();
+        this.text = text.toUpperCase();
     }
 
-    public String encrypt(String text) {
-        String res = "";
-        text = text.toUpperCase();
-        for (int i = 0, j = 0; i < text.length(); i++)
-        {
+    public String encrypt() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0, j = 0; i < text.length(); i++) {
             char c = text.charAt(i);
-            if (c < 'A' || c > 'Z')
+            if (c < 'A' || c > 'Z') {
+                result.append(c);
                 continue;
-            res += (char) ((c + key.charAt(j) - 2 * 'A') % 26 + 'A');
-            j = ++j % key.length();
+            }
+            int keyChar = key.charAt(j) - 'A';
+            result.append((char) ((c + keyChar) % 26 + 'A'));
+            j = (j + 1) % key.length();
         }
-        return res.toString();
+        return result.toString();
     }
 
     public String toStringJson() {
-        return "{ \"result\": \"" + encrypt(null) + "\" }";
+        return "{ \"result\": \"" + encrypt() + "\" }";
     }
- 
-    public static void main(String[] args)
-    {
-        VigenereEncrypt encryptor = new VigenereEncrypt("KEY");
-        System.out.println("Encrypted Text: " + encryptor.encrypt("TEXT"));
+
+    // Tester method
+    public static void main(String[] args) {
+        // Random set of test cases
+        VigenereEncrypt encryptor = new VigenereEncrypt("KEY", "TEXT");
+        System.out.println("Encrypted Text: " + encryptor.encrypt());
     }
 }
