f2538c3c555545ad0a34d7b13deb8b7dfe72e650
willcyber <wutwilliam@gmail.com>
2023-02-26 21:31:58 -0800

    testing more backend

--- src/main/java/com/nighthawk/spring_portfolio/mvc/vigenere/VigenereApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/vigenere/VigenereApiController.java
@@ -1,20 +1,15 @@
 package com.nighthawk.spring_portfolio.mvc.vigenere;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RequestParam;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-// import javax.servlet.http.HttpServletRequest;
-
-// import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/api/vigenc")
@@ -35,12 +30,4 @@ public class VigenereApiController {
         return ResponseEntity.ok(json);
     }
 
-    // add other methods
 }
-
-
-
-
-/** Calendar API
- * Calendar Endpoint: /api/calendar/isLeapYear/2022, Returns: {"year":2020,"isLeapYear":false}
- */
