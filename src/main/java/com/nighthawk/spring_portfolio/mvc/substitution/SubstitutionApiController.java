cd6912d263aee3841761f104aabce64aff8cc631
Saathvika-Ajith <saathvika.ajith@gmail.com>
2023-02-27 18:36:47 -0800

    rhetgew

--- src/main/java/com/nighthawk/spring_portfolio/mvc/substitution/SubstitutionApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/substitution/SubstitutionApiController.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/substitution")
+@RequestMapping("/api/subsc")
 public class SubstitutionApiController {
 
     @GetMapping("all/{text}/{key}")
